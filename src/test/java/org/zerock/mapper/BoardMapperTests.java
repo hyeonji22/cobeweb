876b43db41a6d4c97150670751c2c14899408171
hyeonji22 <rhdguswlx@naver.com>
2023-10-29 21:45:20 +0900

    화면 페이지 처리 계산용 pageDto 만들기 jsp에서 페이지 번호 출력하기

--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -9,8 +9,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
+import org.zerock.domain.PageDTO;
 
 import lombok.extern.log4j.Log4j;
+import oracle.net.aso.p;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -64,6 +66,17 @@ public class BoardMapperTests {
 		
 		list.forEach(b -> log.info(b));
 	
+	}
+	@Test
+	public void testPageDTO() {
+		
+		Criteria cri = new Criteria();
+		cri.setPageNum(25);
+		PageDTO pageDTO = new PageDTO(cri, 251);
+		
+		log.info(pageDTO);
+		
+		
 	}
 	
 }
